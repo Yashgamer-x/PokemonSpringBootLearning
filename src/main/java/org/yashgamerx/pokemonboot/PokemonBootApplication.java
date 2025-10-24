d07edea9dcb80d7e670292086dad4180dae1809f
Yashgamer-x <yashdesai121212@gmail.com>
2025-10-24 17:26:32 -0400

    Changed the log file size to 10 MB. Added Swagger annotation in Main Spring Boot

--- src/main/java/org/yashgamerx/pokemonboot/PokemonBootApplication.java
+++ src/main/java/org/yashgamerx/pokemonboot/PokemonBootApplication.java
@@ -1,12 +1,24 @@
 package org.yashgamerx.pokemonboot;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.yashgamerx.pokemonboot.controller.PokemonController;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Pokemon API",
+                description = "This document contains APIs related to Pokemon",
+                contact = @Contact(
+                        name = "Yash Desai",
+                        email = "yashdesai121212@gmail.com"
+                ),
+                version = "1.0.0"
+        )
+)
 @SpringBootApplication
 public class PokemonBootApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(PokemonBootApplication.class, args);
     }
--- src/main/java/org/yashgamerx/pokemonboot/controller/PokemonController.java
+++ src/main/java/org/yashgamerx/pokemonboot/controller/PokemonController.java
@@ -1,13 +1,11 @@
 package org.yashgamerx.pokemonboot.controller;
 
-import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.yashgamerx.pokemonboot.dao.Pokemon;
-import org.yashgamerx.pokemonboot.dao.PokemonRegion;
 import org.yashgamerx.pokemonboot.dto.PokemonDto;
 import org.yashgamerx.pokemonboot.exception.PokemonException;
 import org.yashgamerx.pokemonboot.exception.PokemonRegionException;
@@ -30,7 +28,7 @@ public class PokemonController {
     }
 
     @GetMapping("/search")
-    public ResponseEntity<Pokemon> searchPokemon(@RequestParam String name) {
+    public ResponseEntity<Pokemon> getPokemon(@RequestParam String name) {
         var pokemonOptional = pokemonService.findPokemonByName(name);
         Pokemon pokemon;
         if (pokemonOptional.isPresent()) {
@@ -45,23 +43,7 @@ public class PokemonController {
                 .body(pokemon);
     }
 
-    /**
-     * Handles HTTP POST requests to add a new Pokémon.
-     * <p>
-     * This method performs the following steps:
-     * <ol>
-     *   <li>Extracts region information from the provided {@link PokemonDto}.</li>
-     *   <li>Attempts to retrieve the corresponding {@link PokemonRegion} using the region name or ID.</li>
-     *   <li>If the region is not found, throws a {@link PokemonRegionException}.</li>
-     *   <li>Creates a new {@link Pokemon} instance using the DTO and resolved region.</li>
-     *   <li>Saves the Pokémon to the database via {@code pokemonService}.</li>
-     *   <li>Returns a success message indicating the Pokémon and its region.</li>
-     * </ol>
-     *
-     * @param pokemonDto the data transfer object containing Pokémon details
-     * @return a success message indicating the Pokémon was added and its region
-     * @throws PokemonRegionException if the region name or ID is invalid or not found
-     */
+
     @PostMapping("/add")
     public @ResponseBody String addPokemon(@RequestBody PokemonDto pokemonDto) {
         var pokemonRegionOptional = pokemonRegionService.getPokemonRegionByDto(pokemonDto);
@@ -75,23 +57,7 @@ public class PokemonController {
         return pokemon.getName()+" was added successfully to "+pokemonRegion.getName()+" Region";
     }
 
-    /**
-     * Handles HTTP PUT requests to update an existing Pokémon.
-     * <p>
-     * This method performs the following steps:
-     * <ol>
-     *   <li>Attempts to find an existing {@link Pokemon} using the name provided in {@link PokemonDto}.</li>
-     *   <li>If the Pokémon is not found, throws a {@link PokemonException}.</li>
-     *   <li>Resolves the target {@link PokemonRegion} using the region name or ID from the DTO.</li>
-     *   <li>Updates the Pokémon's attributes using the provided DTO and resolved region.</li>
-     *   <li>Persists the updated Pokémon to the database via {@code pokemonService}.</li>
-     *   <li>Returns a success message indicating the Pokémon was updated.</li>
-     * </ol>
-     *
-     * @param pokemonDto the data transfer object containing updated Pokémon details
-     * @return a success message confirming the Pokémon update
-     * @throws PokemonException if the Pokémon with the given name is not found
-     */
+
     @PutMapping("/update")
     public @ResponseBody String updatePokemon(@RequestBody PokemonDto pokemonDto){
         var pokemonOptional = pokemonService.findPokemonByNameByDto(pokemonDto);
@@ -108,10 +74,6 @@ public class PokemonController {
         var pokemon = pokemonOptional.orElseThrow(()->new PokemonException("Unable to find Pokemon"));
         pokemonService.deletePokemonById(pokemon.getId());
         return pokemon.getName()+" was removed successfully";
-    }
-
-    public int doMath(){
-        return 1+2;
     }
 
 }
