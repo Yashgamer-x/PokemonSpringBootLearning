817a065c68e18a3de816adddadcc0e2f2e43cb4d
yashgamerx <varoonnoob@gmail.com>
2025-10-24 03:01:52 -0400

    Added Pokemon Type enum

---
+++ src/main/java/org/yashgamerx/pokemonboot/dao/PokemonType.java
@@ -0,0 +1,23 @@
+package org.yashgamerx.pokemonboot.dao;
+
+public enum PokemonType {
+    FIRE,
+    WATER,
+    GRASS,
+    NORMAL,
+    ELECTRIC,
+    FLYING,
+    FIGHTING,
+    POISON,
+    GROUND,
+    PSYCHIC,
+    ROCK,
+    ICE,
+    BUG,
+    DRAGON,
+    GHOST,
+    DARK,
+    STEEL,
+    FAIRY,
+    STELLAR
+}
