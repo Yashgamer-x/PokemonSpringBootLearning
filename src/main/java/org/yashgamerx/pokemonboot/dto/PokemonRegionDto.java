30fecf99db1d48877d7a973cf2d5ebdc23cd2581
Yashgamer-x <yashdesai121212@gmail.com>
2025-10-30 02:34:58 -0400

    Chagned the normal threads to virtual threads for springboot

--- src/main/java/org/yashgamerx/pokemonboot/dto/PokemonRegionDto.java
+++ src/main/java/org/yashgamerx/pokemonboot/dto/PokemonRegionDto.java
@@ -3,9 +3,11 @@ package org.yashgamerx.pokemonboot.dto;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 
+@Builder
 public record PokemonRegionDto(
-        @NotNull @NotBlank
+        @NotBlank
         String name,
         @NotNull @Min(value = 10)
         Integer population
