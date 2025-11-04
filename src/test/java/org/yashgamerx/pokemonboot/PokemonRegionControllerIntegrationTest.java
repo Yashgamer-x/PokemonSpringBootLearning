2f98b8e924a54e8954aeb00df6a9e49975a114ea
Yashgamer-x <yashdesai121212@gmail.com>
2025-11-04 18:56:27 -0500

    Created JUnit & Spring boot Testing for  PokemonRegionControllerIntegrationTest.java. Created PokemonRegionControllerIntegrationTest::baseUrl, PokemonRegionControllerIntegrationTest::setUp, PokemonRegionControllerIntegrationTest::testAddPokemonRegion, PokemonRegionControllerIntegrationTest::testGetPokemonRegionByPopulationFail, PokemonRegionControllerIntegrationTest::testUpdatePokemonRegion.

--- src/test/java/org/yashgamerx/pokemonboot/PokemonRegionControllerIntegrationTest.java
+++ src/test/java/org/yashgamerx/pokemonboot/PokemonRegionControllerIntegrationTest.java
@@ -1,54 +1,96 @@
 package org.yashgamerx.pokemonboot;
 
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.transaction.annotation.Transactional;
-import org.yashgamerx.pokemonboot.dao.PokemonRegion;
 import org.yashgamerx.pokemonboot.dto.PokemonRegionDto;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
-@Transactional
+@DirtiesContext( classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD )
 public class PokemonRegionControllerIntegrationTest {
 
     @LocalServerPort
     private int port;
 
     @Autowired
-    private TestRestTemplate restTemplate;
+    TestRestTemplate restTemplate;
 
-    private String baseUrl() {
-        return "http://localhost:" + port+"/";
+    public String baseUrl() {
+        return "http://localhost:"+port+"/region";
+    }
+
+    @BeforeEach
+    public void setUp(){
+        var pokemonRegion = PokemonRegionDto.builder()
+                .name("Kanto")
+                .population(200)
+                .build();
+        restTemplate.postForEntity(baseUrl()+"/add", pokemonRegion, PokemonRegionDto.class);
     }
 
     @Test
-    void testAddPokemonRegionWhereItIsValid(){
-        var pokemonRegion = PokemonRegion.builder()
-                .name("Kanto")
-                .population(100)
+    public void testAddPokemonRegion() {
+        var pokemonRegionDto = PokemonRegionDto.builder()
+                .name("Johto")
+                .population(500)
                 .build();
-        var response = restTemplate.postForEntity(baseUrl() + "region/add", pokemonRegion, PokemonRegionDto.class);
-        assertEquals(HttpStatus.CREATED, response.getStatusCode());
-        assertNotNull(response.getBody());
+        Assertions.assertNotNull(pokemonRegionDto);
+
+        var pokemonRegionDtoResponseEntity = restTemplate.postForEntity(baseUrl()+"/add", pokemonRegionDto, PokemonRegionDto.class);
+        Assertions.assertEquals(HttpStatus.CREATED, pokemonRegionDtoResponseEntity.getStatusCode());
+
+        var pokemonRegionDtoResponseEntityBodyUnwrapped = pokemonRegionDtoResponseEntity.getBody();
+        Assertions.assertNotNull(pokemonRegionDtoResponseEntityBodyUnwrapped);
+        Assertions.assertEquals(pokemonRegionDto.name(),pokemonRegionDtoResponseEntityBodyUnwrapped.name());
+        Assertions.assertEquals(pokemonRegionDto.population(),pokemonRegionDtoResponseEntityBodyUnwrapped.population());
     }
 
     @Test
-    void testGetPokemonRegionWhereItIsValid(){
-        var pokemonRegion = PokemonRegion.builder()
+    public void testGetPokemonRegionByPopulationFail(){
+        var pokemonRegionDtoResponseEntity = restTemplate.getForEntity(baseUrl()+"/search?population=100", PokemonRegionDto.class);
+        Assertions.assertNotEquals(HttpStatus.OK, pokemonRegionDtoResponseEntity.getStatusCode());
+    }
+
+    @Test
+    public void testGetPokemonRegionByNamePass(){
+        var pokemonRegionDtoResponseEntity = restTemplate.getForEntity(baseUrl()+"/search?name=Kanto", PokemonRegionDto.class);
+        Assertions.assertEquals(HttpStatus.OK, pokemonRegionDtoResponseEntity.getStatusCode());
+
+        var pokemonRegionDtoResponseEntityBodyUnwrapped = pokemonRegionDtoResponseEntity.getBody();
+        Assertions.assertNotNull(pokemonRegionDtoResponseEntityBodyUnwrapped);
+        Assertions.assertEquals("Kanto",pokemonRegionDtoResponseEntityBodyUnwrapped.name());
+        Assertions.assertEquals(200, pokemonRegionDtoResponseEntityBodyUnwrapped.population());
+    }
+
+    @Test
+    public void testUpdatePokemonRegion(){
+        var pokemonRegionDto = PokemonRegionDto.builder()
                 .name("Kanto")
-                .population(100)
+                .population(300)
                 .build();
-        var response = restTemplate.postForEntity(baseUrl() + "region/add", pokemonRegion, PokemonRegionDto.class);
-        assertEquals(HttpStatus.CREATED, response.getStatusCode());
-        assertNotNull(response.getBody());
+        Assertions.assertNotNull(pokemonRegionDto);
+
+        var httpEntity = new HttpEntity<>(pokemonRegionDto);
+        Assertions.assertNotNull(httpEntity);
+
+        var pokemonRegionDtoResponseEntity = restTemplate.exchange(baseUrl()+"/update", HttpMethod.PUT, httpEntity, PokemonRegionDto.class);
+        Assertions.assertEquals(HttpStatus.OK, pokemonRegionDtoResponseEntity.getStatusCode());
+
+        var pokemonRegionDtoResponseEntityBodyUnwrapped = pokemonRegionDtoResponseEntity.getBody();
+        Assertions.assertNotNull(pokemonRegionDtoResponseEntityBodyUnwrapped);
+        Assertions.assertEquals(pokemonRegionDto.name(),pokemonRegionDtoResponseEntityBodyUnwrapped.name());
+        Assertions.assertEquals(pokemonRegionDto.population(),pokemonRegionDtoResponseEntityBodyUnwrapped.population());
     }
 
 }
