de69b97ed6982a06d62036656bd77e426a3a4643
Yashgamer-x <yashdesai121212@gmail.com>
2025-11-13 18:11:01 -0500

    learning spring security

--- src/main/java/org/yashgamerx/pokemonboot/controller/HomeController.java
+++ src/main/java/org/yashgamerx/pokemonboot/controller/HomeController.java
@@ -1,6 +1,7 @@
 package org.yashgamerx.pokemonboot.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,5 +10,15 @@ public class HomeController {
     @GetMapping("/")
     public String getHomePage() {
         return "greeting";
+    }
+
+    @GetMapping("/login")
+    public String getLoginPage() {
+        return "login";
+    }
+
+    @GetMapping("/logout")
+    public String getLogoutPage() {
+        return "logout";
     }
 }
